907144150b7f654850e18b3d040d9af1ac6b2b48
Diego Arturo Parra Molina <48141200+DiegoArturoParra@users.noreply.github.com>
2021-12-23 12:31:47 -0500

    prueba

---
+++ Prueba.Web/Vistas/Gatos.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Gatos.aspx.cs" Inherits="Prueba.Web.Vistas.Gatos" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="BTN_buscar" BackColor="Green" Text="Buscar Gato" OnClick="BTN_buscar_Click" runat="server" />
+        </div>
+        <asp:GridView ID="GV_Gatos" runat="server">
+
+        </asp:GridView>
+        grid
+    </form>
+</body>
+</html>
---
+++ Prueba.Web/Vistas/Gatos.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Prueba.Web.Vistas
+{
+    public partial class Gatos : System.Web.UI.Page
+    {
+       
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected async void BTN_buscar_Click(object sender, EventArgs e)
+        {
+            using (var client = new HttpClient())
+            {
+                using (var response = await client.GetAsync("https://api.thecatapi.com/v1/images/search"))
+                {
+                    string json = await response.Content.ReadAsStringAsync();
+                   
+                }
+            }
+        }
+    }
+}
